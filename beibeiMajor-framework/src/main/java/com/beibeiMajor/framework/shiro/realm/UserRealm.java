242b240b856a7e0d406e0a26c48f62175803d630
wei.cao <wei.cao@downjoy.com>
2020-09-07 17:54:04 +0800

    登录页面添加

--- beibeiMajor-framework/src/main/java/com/beibeiMajor/framework/shiro/realm/UserRealm.java
+++ beibeiMajor-framework/src/main/java/com/beibeiMajor/framework/shiro/realm/UserRealm.java
@@ -1,17 +1,14 @@
 package com.beibeiMajor.framework.shiro.realm;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.beibeiMajor.common.exception.user.*;
+import com.beibeiMajor.common.utils.StringUtils;
+import com.beibeiMajor.framework.shiro.service.SysLoginService;
+import com.beibeiMajor.framework.util.ShiroUtils;
+import com.beibeiMajor.system.domain.SysUser;
+import com.beibeiMajor.system.service.ISysMenuService;
+import com.beibeiMajor.system.service.ISysRoleService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.ExcessiveAttemptsException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
@@ -19,17 +16,9 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.beibeiMajor.common.exception.user.CaptchaException;
-import com.beibeiMajor.common.exception.user.RoleBlockedException;
-import com.beibeiMajor.common.exception.user.UserBlockedException;
-import com.beibeiMajor.common.exception.user.UserNotExistsException;
-import com.beibeiMajor.common.exception.user.UserPasswordNotMatchException;
-import com.beibeiMajor.common.exception.user.UserPasswordRetryLimitExceedException;
-import com.beibeiMajor.framework.shiro.service.SysLoginService;
-import com.beibeiMajor.framework.util.ShiroUtils;
-import com.beibeiMajor.system.domain.SysUser;
-import com.beibeiMajor.system.service.ISysMenuService;
-import com.beibeiMajor.system.service.ISysRoleService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 自定义Realm 处理登录 权限
@@ -83,52 +72,46 @@ public class UserRealm extends AuthorizingRealm
      * 登录认证
      */
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException
-    {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         UsernamePasswordToken upToken = (UsernamePasswordToken) token;
         String username = upToken.getUsername();
         String password = "";
-        if (upToken.getPassword() != null)
-        {
+        //通过host来判断用户俩字来自哪里、web来自前端，admin来自或者空来自后端
+        String host = upToken.getHost();
+        if (upToken.getPassword() != null) {
             password = new String(upToken.getPassword());
         }
 
-        SysUser user = null;
-        try
-        {
-            user = loginService.login(username, password);
+        if (StringUtils.isEmpty(host)) {
+            SysUser user = null;
+            try {
+                user = loginService.login(username, password);
+            } catch (CaptchaException e) {
+                throw new AuthenticationException(e.getMessage(), e);
+            } catch (UserNotExistsException e) {
+                throw new UnknownAccountException(e.getMessage(), e);
+            } catch (UserPasswordNotMatchException e) {
+                throw new IncorrectCredentialsException(e.getMessage(), e);
+            } catch (UserPasswordRetryLimitExceedException e) {
+                throw new ExcessiveAttemptsException(e.getMessage(), e);
+            } catch (UserBlockedException e) {
+                throw new LockedAccountException(e.getMessage(), e);
+            } catch (RoleBlockedException e) {
+                throw new LockedAccountException(e.getMessage(), e);
+            } catch (Exception e) {
+                log.info("对用户[" + username + "]进行登录验证..验证未通过{}", e.getMessage());
+                throw new AuthenticationException(e.getMessage(), e);
+            }
+            SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
+            return info;
+        } else if ("web".equals(host)) {
+            SysUser user = null;
+
+            SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
+            return info;
         }
-        catch (CaptchaException e)
-        {
-            throw new AuthenticationException(e.getMessage(), e);
-        }
-        catch (UserNotExistsException e)
-        {
-            throw new UnknownAccountException(e.getMessage(), e);
-        }
-        catch (UserPasswordNotMatchException e)
-        {
-            throw new IncorrectCredentialsException(e.getMessage(), e);
-        }
-        catch (UserPasswordRetryLimitExceedException e)
-        {
-            throw new ExcessiveAttemptsException(e.getMessage(), e);
-        }
-        catch (UserBlockedException e)
-        {
-            throw new LockedAccountException(e.getMessage(), e);
-        }
-        catch (RoleBlockedException e)
-        {
-            throw new LockedAccountException(e.getMessage(), e);
-        }
-        catch (Exception e)
-        {
-            log.info("对用户[" + username + "]进行登录验证..验证未通过{}", e.getMessage());
-            throw new AuthenticationException(e.getMessage(), e);
-        }
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
-        return info;
+
+        return null;
     }
 
     /**
--- beibeiMajor-web-server/src/main/java/com/beibeiMajor/web/controller/UserController.java
+++ beibeiMajor-web-server/src/main/java/com/beibeiMajor/web/controller/UserController.java
@@ -1,16 +1,83 @@
 package com.beibeiMajor.web.controller;
 
 import com.beibeiMajor.common.core.controller.BaseController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.beibeiMajor.common.core.domain.AjaxResult;
+import com.beibeiMajor.common.utils.ServletUtils;
+import com.beibeiMajor.common.utils.StringUtils;
+import com.beibeiMajor.framework.util.ShiroUtils;
+import com.beibeiMajor.system.domain.SysUser;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 主页和用户登录相关处理
  * @author cw
  */
 @RestController
-@RequestMapping("/test/user")
 public class UserController extends BaseController{
 
+    /**
+     * 登录接口
+     * @param username
+     * @param password
+     * @param rememberMe
+     * @return
+     */
+    @PostMapping("/login")
+    @ResponseBody
+    public AjaxResult ajaxLogin(String username, String password, Boolean rememberMe)
+    {
+        //这个验证器和后台通用，用host来区分是哪个模块的用户，目前暂时使用后台的用户来验证,host-web前端，host-admin为后台
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe,"admin");
+        Subject subject = SecurityUtils.getSubject();
+        try
+        {
+            subject.login(token);
+            return success();
+        }
+        catch (AuthenticationException e)
+        {
+            String msg = "用户或密码错误";
+            if (StringUtils.isNotEmpty(e.getMessage()))
+            {
+                msg = e.getMessage();
+            }
+            return error(msg);
+        }
+    }
+    @GetMapping("/login")
+    public Object login(HttpServletRequest request, HttpServletResponse response)
+    {
+        // 如果是Ajax请求，返回Json字符串。
+        if (ServletUtils.isAjaxRequest(request))
+        {
+            return ServletUtils.renderString(response, "{\"code\":\"1\",\"msg\":\"未登录或登录超时。请重新登录\"}");
+        }
+
+        return new ModelAndView("login");
+    }
+
+    //个人主页
+    @GetMapping({"/index", "/"})
+    public String index(ModelMap mmap) {
+        // 取身份信息
+        SysUser user = ShiroUtils.getSysUser();
+        //没有身份信息，跳转登录
+
+        // 根据用户获取用户个人主页其他信息
+        mmap.addAttribute("list", 1);
+        return "index";
+    }
 }
 
